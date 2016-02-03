bd72a89ce58980c3a63c2c3805551e0d3b839af2
Art Crawford <arthur.crawford@gmail.com>
2016-02-03 09:46:20 +0000

    Added some further complexity to package-c - delegated command line args

--- src/package-c/package-c.py
+++ src/package-c/package-c.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 import requests
+import sys
 
 def main():
     print("Hello from package-c!")
-    print(requests.get("http://www.google.com"))
+    print 'Argument List:', str(sys.argv)
+    r = requests.get("http://www.google.com")
+    if (r.status_code == requests.codes.ok):
+        print("Successful response from http://www.google.com")
+    else:
+        print("Unsucessful response from http://www.google.com")
+    print(r)
 
 if __name__ == "__main__":
     main()
+
